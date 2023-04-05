89253e66d637ec2e4b91b5a3ccd9cd75ae876fcd
Marius <marius.delpeux@gmail.com>
2023-04-05 09:39:11 +0200

    paramètre serveur.js

--- js/server.js
+++ js/server.js
@@ -5,9 +5,13 @@ const PORT = 3137;
 const server = http.createServer(app);
 
 
+app.use((req,res, next)=>{
+    res.setHeader('Content-Type','text/plain');
+    res.status(404).send("Erreur,Page instrouvable");
+});
 app.get('/',(red,res)=>{
     res.send('<h1>Hello world, oui oui baguette</h1>');
-    res.sendFile(__direname+'/page1.html')
+    res.sendFile(__direname+'/index.html')
 });
 
 
